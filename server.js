6daac702445efe18307d52ee1f867506c1d88695
tess-phillips <tess.phillips@me.com>
2023-09-19 13:57:19 +0100

    adding html semantics

--- server.js
+++ server.js
@@ -7,24 +7,56 @@ const posts = [];
 
 server.get("/", (req, res) => {
   const list = posts.map((posts) => {
-    return `<li>${posts.name} | ${posts.post} </li>`;
+    return `<li>
+    <div style="border-style:solid; border-width:0.5px">
+      <span>${posts.name}</span>
+      <p>${posts.post}</p> 
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+    </li>`;
   });
   const html = `
-    <form method="POST">
-      <p>
-        <label for="name">Username</label>
-        <input name="name">
-      </p>
-      <p>
-        <label for="post">Post</label>
-        <textarea name="post"  rows = '5' cols = '33'></textarea>
-      </p>
-      <button>Post</button>
-    </form>
-    <ul>
-      ${list.join("")}
-    </ul>
-  `;
+    <!doctype html>
+      <head>
+        <title>SummerButterfly</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      </head>
+
+      <body>
+
+        <header>
+          <h1>Summer Butterfly</h1>
+        </header>
+
+        <main>
+          <section id="section-form">
+            <form method="POST">
+            <p>
+              <label for="name">Username</label>
+              <input name="name">
+            </p>
+            <p>
+              <label for="post">Post</label>
+              <textarea name="post"  rows = '5' cols = '33'></textarea>
+            </p>
+            <button>Post</button>
+            </form>
+          </section>  
+
+          <section id="section-posts">
+            <ul style="list-style-type:none">
+              ${list.join("")}
+            </ul>
+          </section>  
+        </main>
+
+        <footer>
+
+        </footer>
+      </body>
+    </html>
+    `;
   res.send(html);
 });
 
