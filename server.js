08f77c4c6d40478a172b561164809574e79224c7
ShaughnAnderson94 <andersji94@gmail.com>
2023-09-19 10:30:49 +0100

    deleted extra

--- server.js
+++ server.js
@@ -2,25 +2,6 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send(`<h1>Hello Express</h1>`);
-});
-
-server.get("/colour", (req, res) => {
-  const hex = req.query.hex || "ffffff"; // defaults to white
-  const html = `
-    <style>
-      body {
-        background-color: #${hex};
-      }
-    </style>
-    <form>
-      <label for="hex">Enter hex</label>
-      <input name="hex" value="${hex}">
-    </form>
-  `;
-  res.send(html);
-});
 
 const cheeses = [];
 
