f4bbbaaabbe2ee2e602331c1ef16d0b3fae1d377
ShaughnAnderson94 <andersji94@gmail.com>
2023-09-19 11:00:52 +0100

    port change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require("./server.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
