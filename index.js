d7954e86b33794ba116f658f8ff04a9821dd1182
ShaughnAnderson94 <andersji94@gmail.com>
2023-09-19 11:04:13 +0100

    tweek

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require("./server.js");
 
-const port = process.env.PORT || "8080";
+const PORT = process.env.PORT || "8080";
 server.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
