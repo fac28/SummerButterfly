ae077606d203d567360927dec270f74d149b3d4d
tess-phillips <tess.phillips@me.com>
2023-09-20 15:08:48 +0100

    make-new-posts page working

--- server.js
+++ server.js
@@ -101,57 +101,64 @@ server.post("/", express.urlencoded({ extended: false }), (req, res) => {
 
 
 server.get("/posts", (req, res) => {
-  const list = posts.map((posts) => {
-    return `<div><li>
+  const list = posts.map((post) => {
+    return `<div class="the-post-div">
+    <span>${post.name}</span>
+    <li>
     <p>
-    ${posts.post} 
+    ${post.post} 
     </p>
+    <div class="the-post__the-buttons">
+      <form action="/edit" method="post" class="edit-form">
+        <input type="hidden" name="postIndex" value="${posts.indexOf(post)}">
+        <button type="submit" class="edit-button">Edit</button>
+      </form>
+      <form action="/delete-selected" method="post" class="delete-selected-form">
+        <input type="hidden" name="postIndex" value="${posts.indexOf(post)}">
+        <button type="submit" class="delete-selected-button">Delete</button>
+      </form>
+    </div>
     </li>
-    <button>Edit</button>
-    <button>Delete</button>
-    <span>${posts.name}</span>
     </div>`;
   });
 
   const html = `
-    <!doctype html>
-      <head>
-        <title>SummerButterfly</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
+  <!doctype html>
+    <head>
+      <title>SummerButterfly</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="/style.css">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Inter:wght@300&family=Montserrat&family=Pacifico&family=Roboto&display=swap" rel="stylesheet">
-<link rel="icon" href="public/favicon.png" type="image/png">
-        
+<link rel="icon" type="image/png" href="public/favicon.png">
 </head>
 
-      <body>
+    <body>
 
-        <header>
-          <h1>Summer Butterfly</h1>
-        </header>
+      <header>
+        <h1>Summer Butterfly</h1>
+      </header>
 
-        <main>
-          
+      <main>
+        
+        <section class="section-posts">
+        <form action="/" method="get" >
+          <button class="make-new-posts-button" type="submit" >Make New Posts</button>
+        </form>
 
-          <section class="section-posts">
-            <ul>
-              ${list.join("")}
-            </ul>
-          </section>  
-          <form action="/" method="get" >
-            
-            <button type="submit" >Make New Post</button>
-          
-          </form>
-        </main>
+          <ul>
+            ${list.join("")}
+          </ul>
+        </section>  
+      </main>
 
-        <footer>
-        </footer>
-      </body>
-    </html>
-    `;
+      <footer>
+      <a href="https://www.flaticon.com/free-icons/easter" title="easter icons">Easter icons created by amoghdesign - Flaticon</a>
+      </footer>
+    </body>
+  </html>
+  `;;
   res.send(html);
 });
 
