e7844819226df1602f82e25f0eefbb258eee4895
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-09-20 12:46:52 +0100

    Add test /posts route, successful

--- tests/servers.test.js
+++ tests/servers.test.js
@@ -2,7 +2,7 @@ const test = require("node:test");
 const assert = require("node:assert");
 const server = require("../server");
 
-test("home route return expected page", async () => {
+test("home route returns expected page", async () => {
   const app = server.listen(8080);
   const response = await fetch("http://localhost:8080");
   app.close();
@@ -11,4 +11,14 @@ test("home route return expected page", async () => {
   const body = await response.text();
 
   assert.match(body, /Show Posts Only/);
+});
+
+test("posts route returns posts page", async () => {
+  const app = server.listen(8080);
+  const response = await fetch("http://localhost:8080/posts");
+  app.close();
+
+  assert.equal(response.status, 200);
+  const body = await response.text();
+  assert.match(body, /Make New Post/);
 });
