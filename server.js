137e5eaa8e231f19816da23792e8ff36b3617430
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-09-21 12:27:49 +0100

    Clear comments

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ const posts = [];
 
 server.get("/", (req, res) => {
   const list = posts.map((post) => {
-    //validation must happen here with
-    //post.name
-    //post.post
     if (post.name.length != 0 && post.post.length != 0) {
       return getListTemplate(posts, post);
     }
@@ -49,31 +46,6 @@ server.get("/posts", (req, res) => {
   res.send(html);
 });
 
-// The following code is being worked on for Validaltion:
-// server.post("/", express.urlencoded({ extended: false }), (req, res) => {
-//   const name = sanitize(req.body.name);
-//   const post = sanitize(req.body.post);
-//   const errors = {};
-//   if (!name) {
-//     errors.name = "Please enter your username";
-//   }
-//   if (!post) {
-//     errors.post = "Please enter a post";
-//   }
-//   if (Object.keys(errors).length) {
-//     const body = home(posts, errors, req.body);
-//     res.status(400).send(body);
-//   } else {
-//     posts.push({ name, post });
-//     res.redirect("/");
-//   }
-// });
-
-// server.post("/delete-last", express.urlencoded({ extended: false }), (req, res) => {
-//   posts.pop();
-//   res.redirect("/");
-// });
-
 server.post(
   "/delete-selected",
   express.urlencoded({ extended: false }),
@@ -84,8 +56,7 @@ server.post(
       posts.splice(postIndex, 1); // Remove the post at the specified index
     }
     res.redirect("/");
-  },
+  }
 );
-//hi from Shaughn
+
 module.exports = server;
-// hi from Shaughn
